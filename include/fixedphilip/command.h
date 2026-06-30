d06de6bacef06a9bd346d00b7c6ad0b9486e498f
brokenphilip <13336890+brokenphilip@users.noreply.github.com>
2026-06-30 16:39:03 +0200

    node<T>, command : node<command>

--- include/fixedphilip/command.h
+++ include/fixedphilip/command.h
@@ -1,38 +1,35 @@
 #pragma once
 
+#include <cstring> // strcmp
+
+#include <fixedphilip/node.h>
+
 #include <dpp/dispatcher.h>
 
 namespace fixedphilip
 {
-	class command
+	class command : public node<command>
 	{
 	public:
 		using run_function = void(const dpp::slashcommand_t& event);
 	private:
-		static inline command* first_ = nullptr;
-		static inline command* last_ = nullptr;
-
-		command* prev_ = nullptr;
-		command* next_ = nullptr;
-
 		const char* name_;
 		const char* description_;
 
 		run_function* run_;
 	public:
-		command(const char* name, const char* description, run_function* run);
-		~command();
-
-		inline auto previous() { return prev_; }
-		inline auto next() { return next_; }
+		command(const char* name, const char* description, run_function* run) : name_(name), description_(description), run_(run) {}
+		~command() {}
 
 		inline auto name() { return name_; }
 		inline auto description() { return description_; }
 
 		inline auto run(const dpp::slashcommand_t& event) { run_(event); }
 
-		static inline auto first() { return first_; }
-		static inline auto last() { return last_; }
+		inline virtual bool compare_nodes(command* current, command* next) override final
+		{
+			return strcmp(current->name_, next->name_) < 0;
+		}
 	};
 }
 
---
+++ include/fixedphilip/node.h
@@ -0,0 +1,96 @@
+
+namespace fixedphilip
+{
+	template <typename T>
+	class node
+	{
+	private:
+		static inline T* first_ = nullptr;
+		static inline T* last_ = nullptr;
+
+		T* prev_ = nullptr;
+		T* next_ = nullptr;
+	public:
+		// Sorting function used to compare two nodes and ordering them accordingly.
+		// Return true if "current" should be added before "next", false otherwise.
+		inline virtual bool compare_nodes(T* current, T* next)
+		{
+			return true;
+		}
+
+		inline node()
+		{
+			// if the linked list is empty, we're the first node
+			if (!first_)
+			{
+				first_ = static_cast<T*>(this);
+				last_ = static_cast<T*>(this);
+				return;
+			}
+
+			T* current_iter = first_;
+			T* previous_iter = nullptr;
+			while (current_iter)
+			{
+				// does our node alphabetically precede the currently iterated node?
+				if (compare_nodes(static_cast<T*>(this), current_iter))
+				{
+					// squeeze inbetween the two nodes
+					next_ = current_iter;
+					prev_ = previous_iter;
+
+					// now update the two nodes we squeezed between
+					current_iter->prev_ = static_cast<T*>(this);
+					if (previous_iter)
+					{
+						previous_iter->next_ = static_cast<T*>(this);
+					}
+					else
+					{
+						// no previous iterator - we're the first node
+						first_ = static_cast<T*>(this);
+					}
+					return;
+				}
+
+				// it does not, so proceed with next iteration
+				previous_iter = current_iter;
+				current_iter = current_iter->next_;
+			}
+
+			// we are the last node
+			prev_ = previous_iter;
+			previous_iter->next_ = static_cast<T*>(this);
+			last_ = static_cast<T*>(this);
+		}
+
+		inline ~node()
+		{
+			if (prev_)
+			{
+				prev_->next_ = next_;
+			}
+			else
+			{
+				// no previous node - we're the first
+				first_ = next_;
+			}
+
+			if (next_)
+			{
+				next_->prev_ = prev_;
+			}
+			else
+			{
+				// no following node - we're the last
+				last_ = prev_;
+			}
+		}
+
+		static inline auto first() { return first_; }
+		static inline auto last() { return last_; }
+
+		inline auto previous() { return prev_; }
+		inline auto next() { return next_; }
+	};
+}
