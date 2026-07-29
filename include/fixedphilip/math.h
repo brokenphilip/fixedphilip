c8762eeda7b20ca7a6174a2f4ab57a018e59a39c
brokenphilip <13336890+brokenphilip@users.noreply.github.com>
2026-07-29 16:34:19 +0200

    untested, fix/update area

--- include/fixedphilip/math.h
+++ include/fixedphilip/math.h
@@ -163,14 +163,17 @@ namespace fixedphilip::math
             { "Area",
                 {
                     { "mm²", { "sq.mm", "sqmm", "mm2", "mm^2"/*, "square millimeter", "square millimeters", "square millimetre", "square millimetres"*/ }, micro_to_base, base_to_micro },
-                    { "cm", { "cm", "centimeter", "centimeters", "centimetre", "centimetres" }, centi_to_base, base_to_centi },
-                    { "dm", { "dm", "decimeter", "decimeters", "decimetre", "decimetres" }, deci_to_base, base_to_deci },
+                    { "cm²", { "sq.cm", "sqcm", "cm2", "cm^2"/*, "square centimeter", "square centimeters", "square centimetre", "square centimetres"*/ }, divide<10'000.0>, multiply<10'000.0> },
+                    { "dm²", { "sq.dm", "sqdm", "dm2", "dm^2"/*, "square decimeter", "square decimeters", "square decimetre", "square decimetres"*/ }, centi_to_base, base_to_centi },
                     { "m²", { "sq.m", "sqm", "m2", "m^2"/*, "square meter", "square meters", "square metre", "square metres"*/ } },
+                    { "a", { "a", "are" }, multiply<100.0>, divide<100.0> }
+                    { "ha", { "ha", "hectare" }, multiply<10'000.0>, divide<10'000.0> }
                     { "km²", { "sq.km", "sqkm", "km2", "km^2"/*, "square kilometer", "square kilometers", "square kilometre", "square kilometres"*/ }, mega_to_base, base_to_mega },
 
                     { "in²", { "sq.in", "sqin", "in2", "in^2"/*, "square inch", "square inches"*/ }, multiply<0.00064516>, divide<0.00064516> },
                     { "ft²", { "sq.ft", "sqft", "ft2", "ft^2"/*, "square foot", "square feet"*/ }, multiply<0.09290304>, divide<0.09290304> },
                     { "yd²", { "sq.yd", "sqyd", "yd2", "yd^2"/*, "square yard", "square yards"*/ }, multiply<0.83612736>, divide<0.83612736> },
+                    { "ac", { "ac", "acre" }, multiply<4'046.8564224>, divide<4'046.8564224> },
                     { "mi²", { "sq.mi", "sqmi", "mi2", "mi^2"/*, "square mile", "square miles"*/ }, multiply<2'589'988.110336>, divide<2'589'988.110336> },
 
                     { "Hu²", { "sq.hu", "sqhu", "hu2", "hu^2"/*, "square hammer unit", "square hammer units"*/ }, divide<2'755.55704356>, multiply<2'755.55704356> },
