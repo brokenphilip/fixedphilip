532697ae3de75fd4d5bb3267f99fb9e3b556e63b
brokenphilip <13336890+brokenphilip@users.noreply.github.com>
2026-08-18 21:53:57 +0200

    extract emojis from messages, update discofloor

--- src/modules/utility.cpp
+++ src/modules/utility.cpp
@@ -1,5 +1,7 @@
 #include <discofloor/bot.h>
 
+#include <regex>
+
 namespace discofloor
 {
     class utility_module : public module
@@ -42,11 +44,68 @@ namespace discofloor
             event.reply(dpp::message(std::format(":frame_photo: **| `{}`'s banner is:** {}", user_identified.username, banner)).set_flags(dpp::m_ephemeral));
         }
 
+        static dpp::task<void> run_extract_emojis(const run_event& event)
+        {
+            auto extract_emojis = [](const std::string& message) -> std::vector<std::string>
+            {
+                static std::regex emoji_regex("<(a)?:([^:]+):([0-9]+)>");
+
+                std::vector<std::string> emojis;
+                for (auto it = std::sregex_iterator(message.begin(), message.end(), emoji_regex); it != std::sregex_iterator(); it++)
+                {
+                    auto& smatch = *it;
+
+                    auto animated = smatch[1].matched;
+                    auto name = smatch[2].str();
+                    auto id = smatch[3].str();
+
+                    dpp::emoji emoji(name, id, animated ? dpp::e_animated : 0);
+                    
+                    std::string emoji_extracted = std::format("`<{}:{}:{}>` - <{}>", animated ? "a" : "", name, id, emoji.get_url(4096));
+                    if (std::find(emojis.begin(), emojis.end(), emoji_extracted) == emojis.end())
+                    {
+                        emojis.push_back(emoji_extracted);
+                    }
+                }
+                return emojis;
+            };
+
+            std::regex emoji_regex("<(a)?:([^:]+):([0-9]+)>");
+
+            if (auto message_ctx_menu = event.get_message_context_menu())
+            {
+                auto results = extract_emojis(message_ctx_menu->get_message().content);
+
+                if (results.empty())
+                {
+                    event.reply(dpp::message(":frame_photo: **| No emojis found in this message.**").set_flags(dpp::m_ephemeral));
+                }
+                else
+                {
+                    std::string reply_message = std::format(":frame_photo: **| Extracted {} emoji{} from this message:**", results.size(), results.size() == 1 ? "" : "s");
+                    for (int i = 0; i < results.size(); i++)
+                    {
+                        reply_message += std::format("\n{}. {}", i + 1, results[i]);
+                    }
+                    event.reply(dpp::message(reply_message).set_flags(dpp::m_ephemeral));
+                }
+            }
+            //else if (auto user_ctx_menu = event.get_user_context_menu())
+            //{
+            //    auto user = user_ctx_menu->get_user();
+            //}
+            co_return;
+        }
+
         virtual std::vector<command> commands(bot& bot) override final
         {
             command get_avatar("get avatar", dpp::ctxm_user, bot.me.id, run_get_avatar);
             command get_banner("get banner", dpp::ctxm_user, bot.me.id, run_get_banner);
-            return { get_avatar, get_banner };
+
+            command extract_emojis_message("extract emojis", dpp::ctxm_message, bot.me.id, run_extract_emojis);
+            //command extract_emojis_user("extract emojis", dpp::ctxm_user, bot.me.id, run_extract_emojis);
+
+            return { get_avatar, get_banner, extract_emojis_message };
         }
     public:
         utility_module() : module("utility") {}
